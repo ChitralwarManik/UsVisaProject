83043390e4ee6b5de22b964340217364d08cdc8a
LeoChitralwar <manikchitral123@gmail.com>
2024-06-28 17:12:55 +0530

    added env and requirements file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa_approval",
+    version="0.0.0",
+    author="Manik",
+    author_email="manikchitral123@gmail.com",
+    packages=find_packages()
+)
